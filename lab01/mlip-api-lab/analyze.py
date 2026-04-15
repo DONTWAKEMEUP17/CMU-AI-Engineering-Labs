63f870dd40bc15fe25b98f77ed988fbea6dc4833
calista <cal1sta17.030@gmail.com>
2026-04-14 21:32:57 -0700

    lab01 finished

--- lab01/mlip-api-lab/analyze.py
+++ lab01/mlip-api-lab/analyze.py
@@ -4,9 +4,9 @@ from typing import Any, Dict
 from litellm import completion
 
 # You can replace these with other models as needed but this is the one we suggest for this lab.
-MODEL = "groq/llama-3.3-70b-versatile"
+MODEL = "openai/gpt-4o-mini"
 
-api_key = "hardcoded API_KEY HERE"
+api_key = os.getenv("OPENAI_API_KEY")
 
 
 def get_itinerary(destination: str) -> Dict[str, Any]:
@@ -21,7 +21,32 @@ def get_itinerary(destination: str) -> Dict[str, Any]:
 
     # See https://docs.litellm.ai/docs/ for reference.
 
-    data = ...
-    
+    response = completion(
+        model=MODEL,
+        response_format={ "type": "json_object" },
+        messages=[
+          {"role": "system", 
+           "content": """
+           You are a You are a travel assistant. Return a JSON object with exactly these fields: destination, price_range, ideal_visit_times, top_attractions.
+           - destination: a string representing the travel destination
+           - price_range: a string representing the price range for the destination(e.g., "budget", "mid-range", "luxury")
+           - ideal_visit_times: a string representing the ideal times to visit the destination (e.g., "spring and fall", "year-round", "summer only")
+           - top_attractions: a list of strings representing the top attractions at the destination
+            Make sure to return the response in the specified JSON format without any additional text or explanations.
+           """},
+          {"role": "user", 
+           "content": f"Generate a travel itinerary for the destination: {destination}"}
+        ]
+    )
+
+    data = json.loads(response.choices[0].message.content)
+
+    # Check all required fields exist 
+    required_fields = ["destination", "price_range", "ideal_visit_times", "top_attractions"]
+    for field in required_fields:
+        if field not in data:
+            raise ValueError(f"Missing field: {field}")
+
 
     return data
+
